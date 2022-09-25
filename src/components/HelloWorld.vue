ea01745c1b43b3f2c47c0c7db9490beb8050d8b7
Ornamenta <gliese581@free.fr>
2022-09-25 19:40:02 +0200

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,11 +10,9 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <p>
+      LET's Code LET
+    </p>
   </div>
 </template>
 
